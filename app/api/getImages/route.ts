69a456f22c0e3e143c54b23fe277e296da03f043
Jason Elechi <96956162+jaeee5@users.noreply.github.com>
2023-08-10 00:18:22 +0100

    debugging

--- app/api/getImages/route.ts
+++ app/api/getImages/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
     const response = await fetch(`${process.env.GET_IMAGES}`, {
         cache: "no-store",
@@ -13,4 +15,7 @@ export async function GET(request: Request) {
     });
 };
 
+export async function POST(request: Request) {
+    return NextResponse.json({})
+}
 
--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request){
     //Connect to our Microsoft Azure Functions endpoint
     // add a change to url so it selects the right url to use if in dev environment us localhost otherwise use production url
@@ -8,4 +10,8 @@ export async function GET(request: Request){
     return new Response(JSON.stringify(textData.trim()),{
         status: 200,
     });
+}
+
+export async function POST(request: Request) {
+    return NextResponse.json({})
 }
